e04365ef175dbe7cbb2cfc8b232ed615dbca4ac6
ebpnitesh <nitesh.ebpearls@gmail.com>
2023-09-21 17:31:10 +0545

    Services Component

--- src/data.js
+++ src/data.js
@@ -26,3 +26,24 @@ export const socialLinks = [
   { id: 1, href: "https://www.facebook.com", icon: "fab fa-facebook" },
   { id: 1, href: "https://www.twitter.com", icon: "fab fa-twitter" },
 ];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+];
