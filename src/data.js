a6eb9b6c5459e9d920f2212765a32590b0493049
ebpnitesh <nitesh.ebpearls@gmail.com>
2023-09-22 15:06:33 +0545

    Tours and Footer Component

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+
 export const pageLinks = [
   {
     id: 1,
@@ -45,5 +50,48 @@ export const services = [
     icon: "fas fa-socks fa-fw",
     title: "amazing comfort",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "august 26th, 2023",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod xercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 6,
+    cost: 2550,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "august 26th, 2023",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod xercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 6,
+    cost: 2200,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "august 26th, 2023",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod xercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 6,
+    cost: 3100,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: "august 26th, 2023",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod xercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 6,
+    cost: 3500,
   },
 ];
